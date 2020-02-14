76dde97ec93b52db7d9b71470c0891e5f1b8e8f1
SRINIDHI <srinidhipathy@gmail.com>
2020-02-15 01:26:39 +0530

    commit1

---
+++ Assets/tutorialSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialSample : MonoBehaviour
+{
+    private AndroidJavaObject javaClass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        javaClass = new AndroidJavaObject("com.example.android.mqttlibrary.Helper");
+        javaClass.Call("LogNative");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
